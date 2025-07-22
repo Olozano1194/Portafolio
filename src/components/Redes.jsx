6f17054c9101d4925b4f43543d06c0bee53d5386
Oscar Lozano <olozano1194@gmail.com>
2025-07-22 03:55:04 -0500

    Se agregó un nuevo proyecto a la sección, se agrego la imagen de ese proyecto, se eliminaron los curriculum que no se necesitaban y se agrego el actualizado.

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -9,15 +9,15 @@ function Redes() {
             <p>Hola, soy <span className='text-black font-bold'>Oscar Eduardo Lozano    Bocanegra</span>
             </p>
 
-            <p>Ingeniero de sistemas, graduado de la institución de educación superior      <span className='font-bold'>ITFIP</span>.<br></br> Me he estado especializando en desarrollo Front-End con experiencia en tecnologías como JavaScript, React y Tailwind.<br></br>
-            Explora mis proyectos en GitHub, mi perfil profesional en LinkedIn o descarga mi CV.<br></br>
+            <p>Ingeniero de sistemas, graduado de la institución de educación superior      <span className='font-bold'>ITFIP</span>.<br></br> Me he especializado en desarrollo Front-End, con experiencia en tecnologías como JavaScript, React y Tailwind.<br></br>
+            Puedes Explorar mis proyectos en GitHub, ver mi perfil profesional en LinkedIn o descarga mi CV.<br></br>
             ¡Gracias por visitar! Espero que disfrutes explorando mi trabajo.                
             </p>
 
             <div className='flex items-center justify-center gap-10 p-4 text-4xl'>
                 <Link to={'https://www.linkedin.com/in/OlozanoBocanegra'} target='_blank' className='hover:text-[#0e76a8] redes xl:text-5xl'><FaLinkedin /></Link>
                 <Link to={'https://github.com/Olozano1194'} target='_blank' className='hover:text-[#171515] redes xl:text-5xl'><FaGithub /></Link>
-                <Link to={'curriculum/CurriculumOscarLozanoV2.pdf'} target='_blank' className='hover:text-green-600 redes xl:text-5xl'><FaFile /></Link>
+                <Link to={'curriculum/CurriculumOscarLozanoV3.pdf'} target='_blank' className='hover:text-green-600 redes xl:text-5xl'><FaFile /></Link>
             </div>
                   
         </section>
--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -11,7 +11,7 @@ function SobreMi() {
                 <h2 className='text-4xl font-bold mt-5 mb-4'>Sobre mi</h2>
 
                 <p className='leading-9'>¡Hola¡ Soy <span className='font-bold'>Oscar</span>, ingeniero en sistemas colombiano apasionado por el desarrollo Front-End con habilidades Full Stack. Desde mi graduación en el 2022, me he enfocado en construir una base sólida en programación a través de formación intensiva y proyectos prácticos.<br></br>
-                Mi recorrido incluye:<br></br>✅ Certificación en el programa Oracle ONE (Alura Latam): donde se aprendió de Java, JavaScript, CSS3 y MySQL.<br></br>✅ Programa Misión TIC: fortalecimiento en lógica de programación y mejores prácticas.<br></br>✅ Cursos en Platzi: exploración de frameworks como Bootstrap y Django.<br></br> Actualmente, me estoy especializando en tecnologías modernas como React, Tailwind, Next, Django RestFramework, MongoDB, Vercel y Git.<br></br>
+                Mi recorrido incluye:<br></br>✅ Certificación en el programa Oracle ONE (Alura Latam): donde se aprendió de Java, JavaScript, CSS3 y MySQL.<br></br>✅ Programa Misión TIC: fortalecimiento en lógica de programación y mejores prácticas.<br></br>✅ Cursos en Platzi: exploración de frameworks como Bootstrap y Django.<br></br> Actualmente, me estoy especializando en tecnologías modernas como React, Tailwind, Django RestFramework, MongoDB, Vercel y Git.<br></br>
                 Busco oportunidades para contribuir con soluciones creativas, escalables y centradas en el usuario.<br></br>
                 ¡Gracias por visitar mi perfil y conocer más sobre mí!.
                 </p>
