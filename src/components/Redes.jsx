a469441625149319e24540fe2b6bed2833a724e1
Oscar Lozano <srempalador2021@gmail.com>
2025-12-27 02:25:46 -0500

    docs: Actualizando los estilos del perfil profesional

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -5,14 +5,23 @@ import { FaLinkedin, FaGithub, FaFile  } from 'react-icons/fa';
 
 function Redes() {
     return (
-        <section id='redes' className="redesDiv text-justify text-[1rem] text-slate-950 border-b-2 border-stone-900 p-7 md:text-[1.4rem]">
-            <p>Hola, soy <span className='text-black font-bold'>Oscar Eduardo Lozano    Bocanegra</span>
-            </p>
-            <p>Ingeniero de sistemas<span className='font-bold'>(ITFIP)</span>.<br></br> apasionado por el desarrollo web. Mi enfoque principal es el ecosistema Front-End, donde construyo interfaces dinámicas y funcionales utilizando JavaScript, React y Tailwind CSS.<br></br>
-            Puedes Explorar mis proyectos en GitHub, ver mi perfil profesional en LinkedIn o descarga mi CV.<br></br>
-            Estoy en constante aprendizaje y listo para aportar soluciones técnicas en entornos de desarrollo profesional. ¡Te invito a explorar mis proyectos!.                
-            </p>
-
+        <section id='redes' className="redesDiv text-justify text-[1rem] text-slate-800 border-b-2 border-stone-900 p-7 md:text-[1.3rem]">
+            <article className='font-ubuntu leading-9'>
+                <p>Hola, soy <span className='text-black font-bold'>Oscar Eduardo Lozano    Bocanegra</span>
+                </p>
+                <p>
+                    Ingeniero de sistemas<span className='font-bold'>(ITFIP)</span>, apasionado por el desarrollo web.
+                </p> 
+                <p>
+                    Mi enfoque principal es el ecosistema Front-End, donde construyo interfaces dinámicas y funcionales utilizando JavaScript, React y Tailwind CSS.
+                </p>
+                <p>
+                    Puedes Explorar mis proyectos en GitHub, ver mi perfil profesional en LinkedIn o descarga mi CV.
+                </p>
+                <p>
+                    Estoy en constante aprendizaje y listo para aportar soluciones técnicas en entornos de desarrollo profesional. ¡Te invito a explorar mis proyectos!.                
+                </p>
+            </article> 
             <div className='flex items-center justify-center gap-10 p-4 text-4xl'>
                 <Link to={'https://www.linkedin.com/in/OlozanoBocanegra'} target='_blank' className='hover:text-[#0e76a8] redes xl:text-5xl'><FaLinkedin /></Link>
                 <Link to={'https://github.com/Olozano1194'} target='_blank' className='hover:text-[#171515] redes xl:text-5xl'><FaGithub /></Link>
--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -7,14 +7,26 @@ function SobreMi() {
                 <img src={imgPerfil} alt="img-perfil" className='rounded-full w-60' />
             </div>
 
-            <div className='flex flex-col justify-center items-center text-slate-950 text-[1.05rem] md:text-[1.2rem]'>
+            <div className='flex flex-col justify-center items-center text-slate-800 text-[1.05rem] md:text-[1.3rem]'>
                 <h2 className='text-4xl font-bold mt-5 mb-4'>Sobre mi</h2>
+                <article className='font-ubuntu leading-9'>
+                    <p>
+                        ¡Hola¡ Soy <span className='font-bold'>Oscar</span>, ingeniero de sistemas con una mentalidad orientada a la resolución de problemas y el aprendizaje continuo.
+                    </p>
+                    <p>
+                        Desde mi graduación en 2022, he transformado mi base académica en habilidades técnicas prácticas. Mi formación no se detuvo en la universidad; me he forjado a través de programas de alto rendimiento que me han dado una visión Full Stack:
+                    </p>
+                    <p className='mt-6'>
+                        ✅ Fundamentos Sólidos: A través de Oracle Next Education (Alura) y Misión TIC, consolidé mi lógica de programación en Java y JavaScript, junto con el manejo de bases de datos relacionales (MySQL).
+                    </p>
+                    <p className='mt-5 mb-6'>
+                        ✅ Stack Moderno: Actualmente profundizo en tecnologías de alta demanda como React, MongoDB y Django Rest Framework, buscando siempre escribir código limpio y escalable.
+                    </p>
+                    <p>
+                        Mi objetivo es integrarme a un equipo donde pueda aplicar mi capacidad de análisis, mi adaptabilidad y mis bases técnicas para crear productos que mejoren la experiencia del usuario. Soy un firme creyente de que la ingeniería y el desarrollo web van de la mano para crear soluciones eficientes.
+                    </p>
 
-                <p className='leading-9'>¡Hola¡ Soy <span className='font-bold'>Oscar</span>, ingeniero de sistemas con una mentalidad orientada a la resolución de problemas y el aprendizaje continuo. 
-                Desde mi graduación en 2022, he transformado mi base académica en habilidades técnicas prácticas. Mi formación no se detuvo en la universidad; me he forjado a través de programas de alto rendimiento que me han dado una visión Full Stack:<br></br>
-                ✅ Fundamentos Sólidos: A través de Oracle Next Education (Alura) y Misión TIC, consolidé mi lógica de programación en Java y JavaScript, junto con el manejo de bases de datos relacionales (MySQL).<br></br>✅ Stack Moderno: Actualmente profundizo en tecnologías de alta demanda como React, MongoDB y Django Rest Framework, buscando siempre escribir código limpio y escalable.<br></br>
-                Mi objetivo es integrarme a un equipo donde pueda aplicar mi capacidad de análisis, mi adaptabilidad y mis bases técnicas para crear productos que mejoren la experiencia del usuario. Soy un firme creyente de que la ingeniería y el desarrollo web van de la mano para crear soluciones eficientes.
-                </p>
+                </article>                
             </div>
         </section>
 
